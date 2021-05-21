0af73e8f44f501dfe6c79febcd37be5879d41e0f
omerbinyamin4 <benjomer@post.bgu.ac.il>
2021-05-21 16:39:06 +0300

    21.5 #3

--- hexeditplus.c
+++ hexeditplus.c
@@ -1,6 +1,8 @@
 #define  NAME_LEN  128
 #define  BUF_SZ    10000
 
+
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -24,7 +26,21 @@ struct fun_desc{
     char *name;
     void (*fun)(state*);
 };
-
+char* unit_to_format(int unit, int isDec) {
+    static char* formats_d[] = {"%hhd\n", "%hd\n", "No such unit", "%d\n"};
+    static char* formats_x[] = {"%#hhx\n", "%#hx\n", "No such unit", "%#x\n"};
+    if (isDec) return formats_d[unit-1];
+    else return formats_x[unit-1];
+}
+void print_units(FILE* output, char* buffer, int count, int decFormat, state *s) {
+    char* end = buffer + (s->unit_size)*count;
+    while (buffer < end) {
+        //print ints
+        int var = *((int*)(buffer));
+        fprintf(output, unit_to_format(s->unit_size, decFormat), var);
+        buffer += (s->unit_size);
+    }
+}
 void ToggleDebugMode(state* s){
     if (s->debug_mode){
         s->debug_mode = 0;
@@ -40,9 +56,9 @@ void SetFileName(state* s){
     char fName[128];
     printf("Enter file name:\n");
     fgets(fName, 128, stdin);
-    strcpy(s->file_name, fName);
+    sscanf(fName, "%s", s->file_name);
     if (s->debug_mode)
-        printf("Debug: file name set to '%s'\n", fName);
+        printf("Debug: file name set to '%s'\n", s->file_name);
         
 }
 void SetUnitSize(state* s){
@@ -59,11 +75,43 @@ void SetUnitSize(state* s){
     else printf("Error: invalid number\n");    
 }
 void LoadIntoMemory(state* s){
-    printf("not implemented yet\n");
+    char buffer[BUF_SZ];
+    int input_location, input_length;
+    FILE* file;
+    if (strcmp(s->file_name, "") == 0) {
+        printf("Invalid file name: name cannot be empty\n");
+        return;
+    }
+    file = fopen(s->file_name, "r");
+    if (file == NULL) {
+        perror("Unable to open file: ");
+        return;
+    }
+    printf("Please enter <location> <length>\n");
+    fgets(buffer, BUF_SZ+9, stdin);
+    sscanf(buffer, "%x %d", &input_location, &input_length);
+    if (s->debug_mode)
+        printf("File name: %s, location: %x, length %d\n", s->file_name, input_location, input_length);
+    fseek(file, input_location, SEEK_SET);
+    fread(s->mem_buf, s->unit_size, input_length, file);
+    fclose(file);
+    printf("Loaded %d units into memory\n", input_length);
 }
 void MemoryDisplay(state* s){
-    printf("not implemented yet\n");
-}
+    char buffer[100];
+    int units;
+    int addr;
+    int i;
+    int j;
+    printf("Enter address and length:\n");
+    fgets(buffer, 100, stdin);
+    sscanf(buffer, "%x %d", &addr, &units);
+    printf("Hexadecimal\n===========\n");
+    print_units(stdout, buffer + addr, units, 0, s);
+    printf("\nDecimal\n=======\n");
+    print_units(stdout, buffer + addr, units, 1, s);
+
+    }
 void SaveIntoFile(state* s){
     printf("not implemented yet\n");
 }
